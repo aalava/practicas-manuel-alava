eaa728e57c704d3adfe85251e0531351bb96bb4d
aalava <68528690+aalava@users.noreply.github.com>
2022-07-21 18:03:51 -0500

    Test de autorizacion

--- test.js
+++ test.js
@@ -1,31 +1,46 @@
-import { response } from 'express';
-import fetch from 'node-fetch'
-/*
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer BQC1p4wDVF1JtzW1jSjkoTVh2iIWI4VYH-OdG4zAIMkHove5Kb2ZA89I30UYdqbGQa3Vxvj9aB7tGg3PIcajXWKmJwSuKWbPVQammbL36S9Etl2WWRDjT_xl2TvCbwFO_kBQW-nktEKY28dO4fzLcmVX30Mtyn8OjwNJmTX6Ps5gpsX9KxEDPQu5SBDyjS5Ml8xUFt4Zf9TdPztsMzmM8EqQV6hSkg");
+require('dotenv').config()
+//import 'dotenv/config';
+//import request from 'request';
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
 };
-
-fetch("https://api.spotify.com/v1/me/playlists", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+/*
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
 */
 
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer BQCOVY8EUnduO9A358JnXEehNg_PHjdSH69EXkO__d29iDNsyb75NP-a3qIqlzgT_H8JAzvdd-1a_4lCrRR7m7TPLIoeaXcFNC_0aAMz1viIhy2fEtXPm__ZmfIFZOt9yi_G8ido0xcJSQtdz8PWipqJZfq-q4Op2JPvoYEIxsvQbXFMVhzcXplZniaPHTHc53NyyCY1xjU8SebOqxZk7SJrVAWnsw");
+/*
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch("https://api.spotify.com/v1/browse/new-releases?country=EC&limit=4", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/browse/new-releases?country=EC&limit=20',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
+*/
