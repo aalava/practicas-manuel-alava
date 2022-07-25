e5087276598caef0e7caef29bf73d5498054212d
aalava <manalava@gmail.com>
2022-07-24 23:36:37 -0500

    Promesas

--- spotify2.js
+++ spotify2.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer BQBBxS4Xv8PyDwNnw2SXWQBFoQz6jkli8e1YaeCiXYx7G__7dfPNaJh7OtqpyDNp17-CHjcljwnUwn8o1Vn00CJVorfuFMfNCXBfmlEVHXCZZEYEZns")
+myHeaders.append("Authorization", "Bearer BQD4HE-ZEcIbVaPXQ_f1PF-7Grb1BByjA5SAaQuWIUQo3q20LvKUaEnxGCsPAZAZaIMWLjjanUF8YKLvWLf-4bOgNa14q35h3ZoWNfkCszD-2T7z7Yo")
 
 var requestOptions = {
   method: 'GET',
@@ -10,29 +10,18 @@ var requestOptions = {
 };
 
 /* Ultimos Lanzamientos : Artistas + Albums */
-async function getLastRelease(){
+async function getLastRelease(requestOptions){
   const response = await fetch("https://api.spotify.com/v1/browse/new-releases?country=EC&limit=6", requestOptions)
-  const data = await response.json()
-  
-  console.log(data.albums.items[0].artists[0].name);
-  
-
-
-
+  return new Promise((resolved, reject) => {
+    if (response.status == 200){
+      resolved(response.json());
+    } 
+    else {
+      reject('error')
+    } 
+  })
 }
 
-getLastRelease(requestOptions);
-
-
-/*
-fetch("https://api.spotify.com/v1/browse/new-releases?country=EC&limit=20", requestOptions)
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.albums.limit; i++){
-      console.log('Artista: ' + data.albums.items[i].artists[0].name + ' ' +  'Album: ' + data.albums.items[i].name);
-    }
-
-})
-.catch(error => console.log('error', error));
-
-*/
+getLastRelease(requestOptions)
+.then((res) => console.log(res.albums.limit))
+.catch((err) => console.log(err))
