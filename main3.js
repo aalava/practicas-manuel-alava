78835bb556102a0ae12ba68efdc580bb63cfe621
aalava <68528690+aalava@users.noreply.github.com>
2022-08-05 18:12:57 -0500

    Tracks by Album

--- main3.js
+++ main3.js
@@ -49,9 +49,100 @@ const getAccessToken = async () => {
 
 getAccessToken();
 
+
+async function getTracksByAlbum(AlbumID, requestOptions){
+
+    // Obteniendo el id del div en el documento
+    const tracksAPP = document.getElementById('tracks');
+
+    // Ejecutando la funcion con los parametros
+    const response = await fetch(`${API}/albums/${AlbumID}`, requestOptions);
+    let dataTracksbyAlbum = await response.json();
+
+   
+    const response2 = dataTracksbyAlbum.tracks.items.map((i) => {
+        //const tracksAPP = document.getElementById('tracks');
+
+        const test = i.name;
+
+        console.log(i.name);
+
+        //tracksAPP.append(test);
+
+            
+    })
+
+    let viewTracksByAlbum = `${dataTracksbyAlbum.tracks.items.map((i) => 
+        `
+        <li>${i.name}</li>
+    `).join('')}  
+    `
+
+    tracksAPP.innerHTML = viewTracksByAlbum;
+
+
+    //tracksAPP.innerHTML = viewTracksByAlbum;
+        
+        //console.log(response2);
+    
+    //return response2;        
+
+    /*
+
+    const tracksAPP = document.getElementById('tracks');
+
+    let viewTracksByAlbum = `${dataTracksbyAlbum.tracks.items.map((i) => 
+        `
+        <li>${i.name}</li>
+    `).join('')}  
+    `
+    tracksAPP.innerHTML = viewTracksByAlbum;
+
+*/
+    // Obteniendo el id del div para mostrar los Tracks de los Albums
+    /*const tracksAPP = document.getElementById('tracks');
+
+    let viewTracksByAlbum = `
+        ${dataTracksbyAlbum.tracks.items.map(
+            (i) => 
+                `
+                <div id="standard-modal${orderId}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="standard-modalLabel" aria-hidden="true">
+                    <div class="modal-dialog">  
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title" id="standard-modalLabel"><a href="" target="_blank">${i.name}</a></h4>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <h6>Fecha de Lanzamiento:</h6>
+                                <p></p>
+                                <hr>
+                                <h6>Total de Tracks: ${i.total_tracks}</h6>                               
+                                <p></p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div><!-- /.modal-content -->
+                    </div><!-- /.modal-dialog -->
+                </div><!-- /.modal -->            
+                `
+        ).join('')}
+    `
+
+    // Mostrando los tracks en el div
+    tracksAPP.innerHTML = viewTracksByAlbum;
+
+*/
+}
+
 async function getLastRelease(requestOptions){
     const response = await fetch(`${API}/browse/new-releases?country=EC&limit=18`, requestOptions);
-    const data = await response.json();
+    const dataLastRelease = await response.json();
+    
+    for (let i=0; i < dataLastRelease.albums.limit; i++){
+        await getTracksByAlbum(dataLastRelease.albums.items[i].id, requestOptions);
+    }
     
     /*
     for (let i = 0; i < data.albums.limit; i++){
@@ -60,55 +151,25 @@ async function getLastRelease(requestOptions){
     */
 
     // Listado de Canciones de cada Album
-    async function getTracksByAlbum(AlbumID){
-        const response = await fetch(`${API}/albums/${AlbumID}`, requestOptions);
-        let data = await response.json();
+    
 
-        const response2 = await data.tracks.items.map(i => i.name)
-
-        //console.log(response2[0]);
-        
-        return response2;        
-    }
-
-    const tracksItems = await getTracksByAlbum('6FJxoadUE4JNVwWHghBwnb');
+    //const tracksItems = await getTracksByAlbum('6FJxoadUE4JNVwWHghBwnb', requestOptions);
 
     // Mostrando los Tracks de los Albums
-    const tracksAPP = document.getElementById('tracks');
+    //const tracksAPP = document.getElementById('tracks');
 
     //console.log(tracksItems);
     
-    tracksAPP.innerHTML = tracksItems;
+    //tracksAPP.innerHTML = tracksItems;
 
   
     // Mostrando los últimos lanzamientos 
     const contentAPP = document.getElementById('lastRelease');
 
     let viewLastRelease = `
-        ${data.albums.items.map(
+        ${dataLastRelease.albums.items.map(
             (i, j) => 
                 `
-                <div id="standard-modal${j}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="standard-modalLabel" aria-hidden="true">
-                    <div class="modal-dialog">  
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h4 class="modal-title" id="standard-modalLabel"><a href="${i.external_urls.spotify}" target="_blank">${i.name}</a></h4>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <h6>Fecha de Lanzamiento:</h6>
-                                <p>${i.release_date}</p>
-                                <hr>
-                                <h6>Total de Tracks: ${i.total_tracks}</h6>                               
-                                <p>${tracksItems}</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                            </div>
-                        </div><!-- /.modal-content -->
-                    </div><!-- /.modal-dialog -->
-                </div><!-- /.modal -->
-
                 <div class="col-md-6 col-xl-2">
                     <div class="card"> 
                         <img class="card-img-top img-fluid" src="${i.images[0].url}" alt="Card image cap">
